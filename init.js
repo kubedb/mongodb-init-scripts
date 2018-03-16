f422a24be530332748ef708aba2010013846e957
Maruf Tuhin <maruf.2hin@gmail.com>
2018-03-16 17:39:40 +0600

    Update init.js

--- init.js
+++ init.js
@@ -1,2 +1,2 @@
 db = db.getSiblingDB('kubedb');
-db.people.insert({ "_id" : ObjectId("5aaba447a8d20c024bd32af3"), "_created" : ISODate("2018-03-16T11:02:31.147Z"), "_modified" : ISODate("2018-03-16T11:02:31.147Z"), "firstname" : "kubernetes", "lastname" : "database" });
+db.people.insert({"firstname" : "kubernetes", "lastname" : "database" });
